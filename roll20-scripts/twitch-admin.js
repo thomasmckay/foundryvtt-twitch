d6a93a5e358dc18a38721cb663d0be79f6b5ef3f
Tom McKay <thomasmckay@redhat.com>
2019-07-06 15:56:10 -0400

    wip

--- roll20-scripts/twitch-admin.js
+++ roll20-scripts/twitch-admin.js
@@ -3,7 +3,7 @@
 
 var TwitchAdminCommand = {
     run: function (msg, linkid, args) {
-        var username, character;
+        var username, twitch;
 
         if (args.length < 2) {
             Twitch.rawWrite("Usage: " + this.usage(true), msg.who, "", "twitch admin");
@@ -13,27 +13,43 @@ var TwitchAdminCommand = {
         var objects = findObjs({
             _type: "character",
         });
-        character = _.find(objects, function (obj) {
+        twitch = _.find(objects, function (obj) {
             return obj.get("name") == "Twitch Chat";
         });
-        if (character === undefined) {
+        if (twitch === undefined) {
             Twitch.rawWrite("Error: 'Twitch Chat' character not found", msg.who, "", "twitch admin");
             return;
         }
 
-        sendChat(object.get("name"), args.join(" "));
-
+        var character = args[2],
+            command = args[3];
 
         username = args[1];
         if (args[0] === "add") {
-            user = findObjs({
+            userPermissions = findObjs({
                 _type: "ability",
-                _characterid: character.id,
+                _characterid: twitch.id,
                 name: username
             })[0];
-            var parsed = this.parseAbility(ability.get("action"), msg);
+            var permissions = this.getPermissions(userPermissions.get("action"));
 
+            this.setPermissions(permissions, msg);
         } else if (args[0] === "remove") {
+        } else if (args[0] === "check") {
+            var result;
+
+            if (args.length !== 4) {
+                Twitch.rawWrite("Usage: " + this.usage(true), msg.who, "", "twitch admin");
+                return;
+            }
+
+            userPermissions = findObjs({
+                _type: "ability",
+                _characterid: twitch.id,
+                name: username
+            })[0];
+            result = this.checkPermission(msg, twitch, username, character, command);
+            Twitch.rawWrite("DEBUG: allowed=" + result, msg.who, "", "twitch admin");
         } else {
             Twitch.rawWrite("Error: Unknown admin command '" + args[0] + "'", msg.who, "", "twitch admin");
             return;
@@ -41,7 +57,7 @@ var TwitchAdminCommand = {
     },
 
     usage: function (detailed) {
-        var message = "<b>admin</b> [add|remove] $user [$character $commands]\n";
+        var message = "<b>admin</b> [add|remove|check] $user [$character $commands]\n";
         if (detailed) {
             message += "    $user: Twitch user name\n";
             message += "    $character: First word of character name (or 'All' for general)\n";
@@ -50,15 +66,94 @@ var TwitchAdminCommand = {
         return message;
     },
 
-    parseAbility: function (lines) {
-        var attributes = {};
+    checkPermission: function (msg, twitch, username, character, command) {
+        var userPermissions = findObjs({
+            _type: "ability",
+            _characterid: twitch.id,
+            name: username
+        })[0];
+        var permissions = this.getPermissions(userPermissions.get("action"));
+
+        // Check for specific user under username
+        var permission = undefined;
+        Object.keys(permissions).forEach(function(key) {
+            if (key.toLowerCase().startsWith(character.toLowerCase())) {
+                permission = permissions[key];
+
+            }
+        })
+        if (permission !== undefined && permission.indexOf(command) !== -1) {
+            return true;
+        }
+
+        // Check for 'All' under username
+        Object.keys(permissions).forEach(function(key) {
+            if (key.toLowerCase() === "all") {
+                permission = permissions[key];
+            }
+        })
+        if (permission !== undefined && permission.indexOf(command) !== -1) {
+            return true;
+        }
+
+        // DRY up this
+        var userPermissions = findObjs({
+            _type: "ability",
+            _characterid: twitch.id,
+            name: "All"
+        })[0];
+        var permissions = this.getPermissions(userPermissions.get("action"));
+
+        // Check for specific user under username
+        var permission = undefined;
+        Object.keys(permissions).forEach(function(key) {
+            if (key.toLowerCase().startsWith(character.toLowerCase())) {
+                permission = permissions[key];
+
+            }
+        })
+        if (permission !== undefined && permission.indexOf(command) !== -1) {
+            return true;
+        }
+
+        // Check for 'All' under username
+        Object.keys(permissions).forEach(function(key) {
+            if (key.toLowerCase() === "all") {
+                permission = permissions[key];
+            }
+        })
+        if (permission !== undefined && permission.indexOf(command) !== -1) {
+            return true;
+        }
+
+        return false;
+    },
+
+    getPermissions: function (lines) {
+        var permissions = {};
+        lines.split("\n").forEach( function(line) {
+            var characterCommands = line.split(":");
+            if (characterCommands[0] === undefined) {
+                return;
+            }
+            character = characterCommands[0].trim();
+            permissions[character] = [];
+            characterCommands[1].split(",").forEach(function (command) {
+                permissions[character].push(command.trim());
+            });
+        });
+        return permissions;
+    },
+
+    setPermissions: function (permissions, msg) {
+        var permissions = {};
         lines.split("\n").forEach( function(line) {
             var splitLine = line.split(":");
             if (splitLine[0] === undefined) {
                 return;
             }
-            attributes[splitLine[0]] = splitLine[1].split(",");
+            permissions[splitLine[0]] = splitLine[1].split(",");
         });
-        return attributes;
-    },
+        return permissions;
+    }
 };
